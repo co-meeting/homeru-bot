147b57e38bd6d3631169c83fd3da95b110682a12
hanamizuki10 <mizuki.hana@imprich-create.site>
2021-04-23 14:05:13 +0900

    サンプル関数とともにesLintの設定を更新

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.region('asia-northeast1').https.onRequest((req, res) => {
+  functions.logger.info('Hello logs!', {structuredData: true});
+  res.status(200).send('Hello from Firebase!');
+});
